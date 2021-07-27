88528aaa973611199c310cf39b7861b31af78fb7
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2021-07-27 18:24:24 +0900

    Add : BoardController.java
    docs : BoradController 파일 추가
    author : Hansol
    
    add board/controller/BoardController.java

---
+++ like5/src/main/java/com/kh/like5/board/controller/BoardController.java
@@ -0,0 +1,26 @@
+package com.kh.like5.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+	
+	/**
+	 * [한솔] QnA 게시글 리스트 페이지 
+	 */
+	@RequestMapping("qList.bo")
+	public String qList() {
+		return "board/qna/qnaListView";
+	}
+	
+	/**
+	 * [한솔] QnA 게시글 작성 페이지
+	 */
+	
+	@RequestMapping("qEnrollForm.bo")
+	public String qEnrollForm() {
+		
+		return "board/qna/qnaEnrollForm";
+	}
+}
