ec9bafe8fadf182c05ef62d816b71e779cc56c7d
seongit <seongeunk102@gmail.com>
2021-07-29 23:08:17 +0900

    Docs : Community 전체 리스트 조회
    Docs : Community 전체 리스트 조회
    Author : seongeun
    
    add board/controller/BoardController
    add board/model/dao/BoardDao
    add board/model/service/BoardService
    add board/model/service/BoardServiceImpl
    add views/board/community/comListView.jsp

--- like5/src/main/java/com/kh/like5/board/controller/BoardController.java
+++ like5/src/main/java/com/kh/like5/board/controller/BoardController.java
@@ -1,12 +1,23 @@
 package com.kh.like5.board.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.kh.like5.board.model.service.BoardService;
+import com.kh.like5.board.model.vo.Board;
+import com.kh.like5.common.model.vo.PageInfo;
+import com.kh.like5.common.template.Pagination;
 
 @Controller
 public class BoardController {
 	
+	@Autowired
+	private BoardService bService;
 	
 	//------------------ 한솔-------------------------
 	
@@ -73,8 +84,18 @@ public class BoardController {
 	 * @author seong 
 	 */
 	@RequestMapping("comList.bo")
-	public ModelAndView comList(ModelAndView mv){
+	public ModelAndView comList(@RequestParam(value="currentPage",defaultValue="1")int currentPage,ModelAndView mv){
+		
+		int listCount = bService.comListCount();
+		
+		PageInfo pi = Pagination.getPageInfo(listCount,currentPage,5,5);
+		
+		ArrayList<Board>comList = bService.comList(pi);
+
+		mv.addObject("comList",comList);
+		mv.addObject("pi",pi);
 		mv.setViewName("board/community/comListView");
+		
 		return mv;
 	}
 	
