725d089e154cf1dc4c3d867d165d3f903e9220e0
wondongkyu <84184691+wondongkyu@users.noreply.github.com>
2021-08-02 03:55:15 +0900

    <docs> : 마이페이지 게시글 메뉴 기능 추가, 회원 정보 수정 기능추가 author : dongKyu

--- like5/src/main/java/com/kh/like5/member/model/dao/MemberDao.java
+++ like5/src/main/java/com/kh/like5/member/model/dao/MemberDao.java
@@ -1,10 +1,16 @@
 package com.kh.like5.member.model.dao;
 
+import java.util.ArrayList;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.kh.like5.admin.model.vo.Calculate;
+import com.kh.like5.board.model.vo.Board;
+import com.kh.like5.board.model.vo.Reply;
 import com.kh.like5.member.model.vo.Customer;
 import com.kh.like5.member.model.vo.Member;
+import com.kh.like5.member.model.vo.Sponsorship;
 
 @Repository
 public class MemberDao {
@@ -23,5 +29,45 @@ public class MemberDao {
 		return sqlSession.insert("memberMapper.insertInquiry",c);
 	}
 	
+	
+	public ArrayList<Board> newlylist(SqlSessionTemplate sqlSession, int memNo) {
+		
+		
+		return (ArrayList)sqlSession.selectList("memberMapper.newlylist", memNo);
+		
+				
+	}
+
+	public ArrayList<Board> tempSavelist(SqlSessionTemplate sqlSession, int memNo) {
+		
+		return (ArrayList)sqlSession.selectList("memberMapper.tempSaveList", memNo);
+		
+	}
+
+	public ArrayList<Reply> anslist(SqlSessionTemplate sqlSession, int memNo) {
+		
+		return (ArrayList)sqlSession.selectList("memberMapper.ansList", memNo);
+		
+	}
+
+	public ArrayList<Sponsorship> price(SqlSessionTemplate sqlSession, int memNo) {
+		
+		return (ArrayList)sqlSession.selectList("memberMapper.Spon", memNo);
+		
+	}
+
+
+	public ArrayList<Calculate> calculate(SqlSessionTemplate sqlSession, int memNo) {
+		
+		return (ArrayList)sqlSession.selectList("memberMapper.Calculate", memNo);
+		
+	}
+
+
+	public int updateMember(SqlSessionTemplate sqlSession, Member m) {
+		
+		return sqlSession.insert("memberMapper.updateMember",m);
+		
+	}
 	
 }
